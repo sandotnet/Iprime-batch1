f10821f9877d00a9eef4b344a01f822ed06a7be1
sandotnet <87011674+sandotnet@users.noreply.github.com>
2024-01-02 17:32:51 +0530

    commit_2_12

---
+++ Asp.net Core/HandsOn/MyFirstAPI/Controllers/StudentController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MyFirstAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StudentController : ControllerBase
+    {
+        string[] students = new string[] { "Arya", "Kavya", "Manoj", "Faris" };
+        //action methods or endpoints
+        [HttpGet("GetAllStudents")]
+        public string[] GetStudents()
+        {
+            return students;
+        }
+        [HttpGet("GetStudentById/{id}")]
+        public string GetStudent(int id)
+        {
+            return students[id - 1];
+        }
+        [HttpPost("AddStudent/{name}")]
+        public string Add(string name)
+        {
+            students[0] = name;
+            return "Student Added";
+        }
+        [HttpPut("{name}")]
+        public string Update(string name)
+        {
+            return "Student Updaed";
+        }
+        [HttpDelete("{id}")]
+        public string Delete(int id)
+        {
+            return "Student Deleted";
+        }
+    }
+}
---
+++ Asp.net Core/HandsOn/MyFirstAPI/Controllers/TeacherController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MyFirstAPI.Controllers
+{
+    [Route("api/[controller]")] //Route specifies url patterns of a controller
+    [ApiController] //attrite
+    public class TeacherController : ControllerBase
+    {
+        //action method or endpoints
+        [HttpGet("Greet")] //this method is invoked by get request
+        //request  http://localhost:50660/api/Teacher/Greet
+        public string Greet()
+        {
+            return "Good Morning Teachers"; //sent this message as a http response message
+        }
+        [HttpGet("GetAll")]
+        //request: http://localhost:50660/api/Teacher/GetAll
+        public string[] GetTeachers()
+        {
+            return new string[] { "Roopam","Sanu","Santhosh","Karthik"};
+        }
+        [HttpGet]
+        public string Hello()
+        {
+            return "Hello World!!!";
+        }
+        [HttpGet("GetTeachersByStd/{std}")] //route with parameter
+        // http://localhost:50660/api/Teacher/GetTeachersByStd/2
+        public string[] GetTeacherByClass(string std)
+        {
+            return new string[] { "Roopam", "Sanu", "Santhosh", "Karthik","Ahila" };
+        }
+    }
+}
