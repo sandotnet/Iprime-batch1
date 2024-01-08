680bdeb1ef03d59b158f5d70c2fde65db2721d20
sandotnet <87011674+sandotnet@users.noreply.github.com>
2024-01-08 10:58:54 +0530

    commit_18_24

---
+++ Asp.net Core/HandsOn/HandsOnAPIUsingModelValidation/Controllers/AccountController.cs
@@ -0,0 +1,61 @@
+﻿using HandsOnAPIUsingModelValidation.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HandsOnAPIUsingModelValidation.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AccountController : ControllerBase
+    {
+        List<Login> logins = new List<Login>()
+        {
+            new Login(){Username="Anna",Password="12345"},
+            new Login(){Username="Merry",Password="12345"},
+            new Login(){Username="Manoj",Password="12345"},
+            new Login(){Username="Chandra",Password="12345"},
+        };
+        List<User> users = new List<User>()
+        {
+            new User(){Id=303,Name="Manoj",Email="manoj@gmail.com",Mobile="9890989090",Username="Manoj",Password="12345"}
+        };
+        [HttpPost]
+        public IActionResult Validate(Login login)
+        {
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    //var user = logins.SingleOrDefault(u => u.Username == login.Username && u.Password == login.Password);
+                    //var user = (from u in logins
+                    //            where u.Username == login.Username && 
+                    //            u.Password == login.Password
+                    //            select u).SingleOrDefault();
+                    var user = (from l in logins
+                                join u in users
+                                on l.Username equals u.Username
+                                where u.Username == login.Username &&
+                                u.Password == login.Password
+                                select u).SingleOrDefault();
+                    if (user != null)
+                    {
+                        return StatusCode(200, user);
+                    }
+                    else
+                    {
+                        return StatusCode(200, new JsonResult("Invalid User Credentials"));
+                    }
+                }
+                else
+                {
+                    return BadRequest();
+                }
+            }
+            catch (Exception ex)
+            {
+
+                return StatusCode(501, ex);
+            }
+        }
+    }
+}
--- LINQ/HandsOn/HandsOnLinq/Demo9.cs
+++ LINQ/HandsOn/HandsOnLinq/Demo9.cs
@@ -23,14 +23,14 @@ namespace HandsOnLinq
             s = students.Last();
             s = students.Where(s => s.Name.Length > 4).Last();
             Console.WriteLine(s.Name);
-            s = students.Where(s => s.Name.Length > 14).FirstOrDefault();
+            s = students.Where(s => s.Name.Length > 14).FirstOrDefault(); //return null when expression return no records
             if (s == null)
                 Console.WriteLine("No Students");
-            s = students.Where(s => s.Name.Length > 14).LastOrDefault();
+            s = students.Where(s => s.Name.Length > 14).LastOrDefault(); //return null when expression return no records
             //Single use only when expression return one record
             //It throws exception when expression return more than one record or no records
             s = students.Single(s => s.Id == 3);
-            //Single use only when expression return one record
+            //SingleOrDefault use only when expression return one record
             //It returns default when expression retrun no records
             //It throws exception when expression return more than one record 
             s = students.SingleOrDefault(s => s.Id == 12);
@@ -39,6 +39,11 @@ namespace HandsOnLinq
             else
                 Console.WriteLine("Invalid Id");
             s = students.SingleOrDefault(s => s.Name == "Naren" && s.Id == 5);
+
+            List<Student> list1 = students.Where(s => s.Name.Length > 4).ToList(); //convert IEnumarable data to List Type
+            Student[] students1 = students.Where(s => s.Name.Length > 4).ToArray(); //convert IEnumable data to Array
+            Student s1 = students.Where(s => s.Name.Length > 4).ElementAt(0); //return 1st index record
+            s1 = students.Where(s => s.Name.Length > 4).ElementAt(1); //return 2nd record
 
         }
     }
